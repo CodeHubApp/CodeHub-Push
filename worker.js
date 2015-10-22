f8750b5d282e566232f31bbdcf2c61792b173af9
Dillon Buchanan <thedillonb@gmail.com>
2015-10-21 21:19:02 -0400

    Fix the Commit notification

--- worker.js
+++ worker.js
@@ -108,7 +108,8 @@ function processNotification(client, notification, callback) {
               // Get the number from the URL & create a short version for display
               var num = notification.subject.url.substring(notification.subject.url.lastIndexOf('/') + 1);
               var shortNum = num.substring(0, 6);
-              msg = body.author.login + ' commented on ';
+	      var bodyUser = body.user || body.author;
+              msg = bodyUser.login + ' commented on ';
               msg += ' commit';
               msg += ' ' + notification.repository.full_name + '@' + num.substring(0, 6);
               data['c'] = num;
